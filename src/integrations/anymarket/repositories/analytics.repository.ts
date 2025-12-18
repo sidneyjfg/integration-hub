32388c1353f928b7ec579df7589e1c386d4befd4
Sidney Junio <sidney.junio@nerus.com.br>
2025-12-18 17:43:12 -0300

    Ajusta select resumo

--- src/integrations/anymarket/repositories/analytics.repository.ts
+++ src/integrations/anymarket/repositories/analytics.repository.ts
@@ -10,37 +10,25 @@ export async function BuscaInsightsdePedidoAnyXNerus(): Promise<any[]> {
 
   const sql = `
 SELECT
-    t.order_id                                AS order_anymarket,
-    e.ordno                                   AS order_erp,
-    t.marketplace_id                          AS codigo_anymarket,
-    ec.storeno                                AS loja_erp,
-    t.total                                   AS total_anymarket,
-    ec.total_erp,
+    t.order_id                         AS order_anymarket,
+    e.ordno                            AS order_erp,
+    t.total                            AS total_anymarket,
+    SUM(ec.vTotal) / 100               AS total_erp,
     CASE
         WHEN t.order_id IS NOT NULL AND e.ordno IS NOT NULL THEN 'AMBOS'
         WHEN t.order_id IS NOT NULL AND e.ordno IS NULL THEN 'SÓ ANYMARKET'
         WHEN t.order_id IS NULL AND e.ordno IS NOT NULL THEN 'SÓ ERP'
     END AS situacao
 FROM ${coreConfig.DB_NAME_MONITORAMENTO}.temp_orders t
-LEFT JOIN (
-    SELECT
-        ordnoWeb,
-        ordno,
-        storeno,
-        SUM(vTotal) / 100 AS total_erp
-    FROM ${coreConfig.DB_NAME_DADOS}.eordchannelp
-    WHERE storeno IN (${storenos})
-    GROUP BY
-        ordnoWeb,
-        ordno,
-        storeno
-) ec
+LEFT JOIN ${coreConfig.DB_NAME_DADOS}.eordchannelp ec
     ON ec.ordnoWeb = t.order_id
-   AND ec.storeno = t.marketplace_id
 LEFT JOIN ${coreConfig.DB_NAME_DADOS}.eord e
     ON e.ordno = ec.ordno
-   AND e.storeno = ec.storeno
-WHERE t.marketplace_id IN (${storenos});
+    and e.storeno = ec.storeno
+GROUP BY
+    t.order_id,
+    e.ordno,
+    t.total
 `
 
   try {
