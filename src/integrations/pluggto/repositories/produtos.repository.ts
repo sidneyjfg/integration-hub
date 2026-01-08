1cb2235a38d2ba2dbfc2fe99a8667f1edba89ee6
Sidney Junio <sidney.junio@nerus.com.br>
2026-01-08 08:58:48 -0300

    Fix: Ajuste no insert Into de produtos

--- src/integrations/pluggto/repositories/produtos.repository.ts
+++ src/integrations/pluggto/repositories/produtos.repository.ts
@@ -5,8 +5,15 @@ import { PluggtoProductBody } from '../../../shared/types'
 
 export async function salvarProdutosTempPluggto(produtos: PluggtoProductBody[]) {
   const sql = `
-    INSERT INTO ${coreConfig.DB_NAME_MONITORAMENTO}.temp_products (idNerus, idPluggto, ean, name, price, pricePromotion, stock)
+    INSERT INTO ${coreConfig.DB_NAME_MONITORAMENTO}.temp_products
+      (idNerus, idPluggto, ean, name, price, pricePromotion, stock)
     VALUES (?, ?, ?, ?, ?, ?, ?)
+    ON DUPLICATE KEY UPDATE
+      ean = VALUES(ean),
+      name = VALUES(name),
+      price = VALUES(price),
+      pricePromotion = VALUES(pricePromotion),
+      stock = VALUES(stock)
   `
 
   for (const p of produtos) {
