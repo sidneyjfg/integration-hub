35f2c91d4cc258d918a30f18a77c1c41899c7dc1
sidneyjfg <sidneyjunio191@gmail.com>
2026-03-02 15:20:03 -0300

    Fix: ajuste query

--- src/integrations/pluggto/repositories/produtos.repository.ts
+++ src/integrations/pluggto/repositories/produtos.repository.ts
@@ -5,10 +5,12 @@ import { coreConfig } from '../../../core/env.schema'
 import { PluggtoProductBody } from '../../../shared/types'
 
 export async function salvarProdutosTempPluggto(produtos: PluggtoProductBody[]) {
+  if (!produtos.length) return
+
   const sql = `
     INSERT INTO ${coreConfig.DB_NAME_MONITORAMENTO}.temp_products
       (idNerus, idPluggto, ean, name, price, pricePromotion, stock)
-    VALUES (?, ?, ?, ?, ?, ?, ?)
+    VALUES ?
     ON DUPLICATE KEY UPDATE
       ean = VALUES(ean),
       name = VALUES(name),
@@ -17,15 +19,15 @@ export async function salvarProdutosTempPluggto(produtos: PluggtoProductBody[]) 
       stock = VALUES(stock)
   `
 
-  for (const p of produtos) {
-    await poolMonitoramento.execute<ResultSetHeader>(sql, [
-      p.idNerus,
-      p.idPluggto,
-      p.ean,
-      p.name,
-      p.price,
-      p.pricePromotion,
-      p.stock,
-    ])
-  }
+  const values = produtos.map((p) => [
+    p.idNerus,
+    p.idPluggto,
+    p.ean,
+    p.name,
+    p.price,
+    p.pricePromotion,
+    p.stock,
+  ])
+
+  await poolMonitoramento.query<ResultSetHeader>(sql, [values])
 }
